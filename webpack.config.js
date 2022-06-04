0b715c249d72e0d00990c94b7f3b11fc74987c89
Kelsey Alderman <kelseyalderman@gmail.com>
2022-06-04 13:17:27 -0700

    Revert "revert"
    This reverts commit bfe5dce94e17c4e03323f1efa722dcc1e59d961d.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const path = require("path");
 const webpack = require("webpack");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
